d69024220e6843c6348564c205d75d3919043b6c
Ken <woow44410@gmail.com>
2024-10-11 14:41:46 +0800

    main

--- next.config.js
+++
@@ -1,6 +0,0 @@
-// next.config.js
-module.exports = {
-  output: 'export',  // 設定為靜態導出
-  basePath: '/my-blog', // 如果你的 GitHub Pages URL 是子目錄，請設置 basePath
-  assetPrefix: '/my-blog/', // 如果你的 GitHub Pages URL 是子目錄，請設置 assetPrefix
-}
