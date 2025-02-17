783237bf08cc9dea72b83b1f9df36456d5a7b871
woow44410 <woow44410@gmail.com>
2025-02-17 09:58:32 +0800

    Configure for GitHub Pages deployment

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,18 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+
+const nextConfig = {
+  output: 'export',  // 添加這行
+  basePath: '/self_introduction', // 添加這行，使用您的倉庫名稱
+  images: {
+      unoptimized: true, // 如果使用了 Next.js 的 Image 組件，需要添加這行
   },
 };
 
-export default config;
+export default nextConfig;
